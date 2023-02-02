3f3bdb0b3f535b4db8d171ad8f29c6201031f9ac
Eddie Li (ADZ) <eddie.li.624@gmail.com>
2023-02-03 01:40:23 +0800

    FIX

--- lib/export_to/exporter/xlsx.rb
+++ lib/export_to/exporter/xlsx.rb
@@ -3,17 +3,46 @@ module ExportTo
     class Xlsx < Struct.new(:rows)
 
       def export
-        workbook = FastExcel.open(constant_memory: true)
-        worksheet = workbook.add_worksheet("Default")
-
-        bold = workbook.bold_cell_format
-        worksheet.set_row(0, FastExcel::DEF_COL_WIDTH, bold)
-
         rows.each! do |columns, model, x|
           worksheet.write_row(x, columns)
         end
 
         workbook.read_string
+      end
+
+      protected
+
+      def workbook
+        @workbook ||= FastExcel.open(constant_memory: true)
+      end
+
+      def worksheet
+        @worksheet ||= begin
+          ws = workbook.add_worksheet(options.fetch(:worksheet) { "Default" })
+          ws.auto_width = true if auto_width?
+          ws.set_row(0, height, bold)
+          ws
+        end
+      end
+
+      def bold
+        workbook.bold_cell_format
+      end
+
+      def options
+        @options ||= (rows.options || {})
+      end
+
+      def worksheet_name
+        options.fetch(:worksheet) { "Default" }
+      end
+
+      def auto_width?
+        options.fetch(:auto_width) { true }
+      end
+
+      def height
+        options.fetch(:height) { 10 }
       end
 
     end
